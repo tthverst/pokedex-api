5294ea78507315e9c6e04d61d1189ddcba83b563
Dennis <djcprinse@users.noreply.github.com>
2017-03-28 14:03:34 +0200

    Using mLab database
    Using the mLab database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 
+// Localhost
+// module.exports = function () {
+//     mongoose.Promise = global.Promise;
+//     mongoose.connect('mongodb://localhost:27017/pokeapi');
+//     return mongoose;
+// };
+
+// mLab
 module.exports = function () {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost:27017/pokeapi');
+    mongoose.connect('mongodb://ash:catchpokemons@ds115110.mlab.com:15110/pokeapi');
     return mongoose;
 };
