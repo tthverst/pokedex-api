cc2449ac3ff095e01cf00f2dc8ad67e48229791e
Dennis <djcprinse@users.noreply.github.com>
2017-04-03 19:20:11 +0200

    Beginning with test
    Starting with tests

--- tests/pokemontest.js
+++ tests/pokemontest.js
@@ -2,48 +2,45 @@ var request = require('supertest');
 var expect = require('chai').expect;
 var should = require('chai').should();
 
-var app = require('./app.js');
+var app = require('../app.js');
 
-function makeRequest(route, statusCode, done){
+function makeRequest(route, statusCode, done) {
 	request(app)
 		.get(route)
+		.set({ 'Content-Type': 'application/json' })
 		.expect(statusCode)
-		.end(function(err, res){
-			if(err){ return done(err); }
+		.end(function (err, res) {
+			if (err) { return done(err); }
 
 			done(null, res);
 		});
 };
 
-describe('Testing pokemon route', function(){
-	describe('without params', function(){
-		it('should return pokemons', function(done){
-			var today = new Date();
-			var expectedString = 
-				(today.getDate() < 10 ? '0' : '') + 
-				today.getDate() + '-' + 
-				(today.getMonth() + 1 < 10 ? '0' : '') + 
-				(today.getMonth() + 1) + '-' + 
-				today.getFullYear();
+describe('Testing pokemon route', function () {
+	describe('without params', function () {
+		it('should return pokemons', function (done) {
 
-			makeRequest('/', 200, function(err, res){
-				if(err){ return done(err); }
-
-				expect(res.body).to.have.property('date');
-				expect(res.body.date).to.not.be.undefined;
-				expect(res.body.date).to.equal(expectedString);
+			makeRequest('/pokemons', 200, function (err, res) {
+				if (err) { return done(err); }
+				console.log(res.body);
 				done();
 			});
 		});
 	});
 
-	describe('with invalid params', function(){
-		it('should return 400 when date is invalid', function(done){
-			makeRequest('/35/2/2000', 400, done);
+	describe('with invalid name param', function () {
+		it('should return 404 when name is invalid', function (done) {
+			makeRequest('/pokemons/bulbasaurr', 404, function (err, res) {
+				if (err) { return done(err); }
+				done();
+			});
 		});
 
-		it('should return 400 when date is not numeric', function(done){
-			makeRequest('/test/me/now', 400, done);
+		it('should return 200 when name is valid', function (done) {
+			makeRequest('/pokemons/bulbasaur', 200, function(err, res) {
+				if (err) { return done(err); }
+				done();
+			});
 		});
 	});
 });
