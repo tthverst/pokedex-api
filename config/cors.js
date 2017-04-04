562836c08d2df2a4ce095c744d7426716cadce81
Dennis <djcprinse@users.noreply.github.com>
2017-04-04 23:51:29 +0200

    Cors

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
     next();
   };
 }
