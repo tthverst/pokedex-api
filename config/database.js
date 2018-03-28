28be6b36ba8c394e4c922b486b6a0f8a3860c096
Tom Verstappen <tthverst@avans.nl>
2018-03-28 15:42:44 +0200

    fix 2

--- config/database.js
+++ config/database.js
@@ -1,22 +1,8 @@
 var mongoose = require('mongoose');
 
-/* // Localhost
-module.exports = function () {
-	mongoose.Promise = global.Promise;
-	mongoose.connect('mongodb://localhost:27017/pokeapi');
-	return mongoose;
-}; */
-
- // mLab production
+// mLab production
 module.exports = function () {
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://ash:catchpokemons@ds111319.mlab.com:11319/pokeapi');
     return mongoose;
 };
-/*
-//mLab development
-module.exports = function () {
-      mongoose.Promise = global.Promise;
-      mongoose.connect('mongodb://ash:catchpokemon@ds151060.mlab.com:51060/pokeapi-test');
-      return mongoose;
- };
